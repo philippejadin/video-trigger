a97bd7ffcc11c7a1f24f775e11f8e7e04b50440d
Philippe Jadin <philippe.jadin@gmail.com>
2018-06-13 13:51:43 +0200

    prod install script (wip)

---
+++ install_production.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Error out if anything fails.
+set -e
+
+# Make sure script is run as root.
+if [ "$(id -u)" != "0" ]; then
+  echo "Must be run as root with sudo! Try: sudo ./install_production.sh"
+  exit 1
+fi
+
+
+sudo bash install.sh
+sudo bash enable.sh
+
+cd /tmp
+wget https://raw.githubusercontent.com/janztec/empc-arpi-linux-readonly/master/install-experimental.sh -O install-experimental.sh
+sudo bash install-experimental.sh
